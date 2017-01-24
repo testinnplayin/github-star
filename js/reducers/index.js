0492c5982b44d9b1e4b0dfeb803d1083bb6dd14a
R Wood <archaurore@gmail.com>
2017-01-24 13:05:34 -0500

    added add repo and rate repo to reducers/index.js

---
+++ js/reducers/index.js
@@ -0,0 +1,28 @@
+import * as actions from '../actions/index';
+
+const initialRepositoryState = [];
+
+export const repositoryReducer = (state=initialRepositoryState, action) => {
+	if (action.type === actions.ADD_REPOSITORY) {
+		return [...state, {
+			name: action.repository,
+			rating: null
+		}];
+	} else if (action.type === actions.RATE_REPOSITORY) {
+		const index = state.findIndex(repository => {
+			repository.name === action.repository;
+		});
+
+		if (index === -1) {
+			throw new Error('Could not find repository');
+		}
+
+		const before = state.slice(0, index);
+		const after = state.slice(index + 1);
+
+		const newRepository = Object.assign({}, state[index], {rating: action.rating});
+		return [...before, newRepository, ...after];
+	}
+	return state;
+};
+
