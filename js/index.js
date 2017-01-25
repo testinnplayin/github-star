3c94b593d0759cbd2a03a9a42a223f5563024345
R Wood <archaurore@gmail.com>
2017-01-25 17:52:23 -0500

    added render method to index.js in root js/

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RepositoryList from './components/repository-list';
+
+document.addEventListener('DOMContentLoaded', () =>
+	ReactDOM.render(<RepositoryList />, document.getElementById('app'));
+);
