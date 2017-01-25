1971689d7e15debfd4597795c65f2afafb607618
R Wood <archaurore@gmail.com>
2017-01-25 17:58:32 -0500

    added react-redux provider to index.js at js/

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@ require('babel-polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
+import store from './store';
 import RepositoryList from './components/repository-list';
 
 document.addEventListener('DOMContentLoaded', () =>
-	ReactDOM.render(<RepositoryList />, document.getElementById('app'));
+	ReactDOM.render(
+		<Provider store={store}>
+			<RepositoryList />
+		</Provider>
+		, document.getElementById('app'));
 );
